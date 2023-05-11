ba451093a365124a83dc99253d837e788cbc0e05
Joanne Ho <cyh66368@gmail.com>
2023-05-11 13:53:36 -0700

    implement logic to verify user email from the password reset form

--- resetPassword.js
+++ resetPassword.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const { database } = require("./databaseConnection");
 require("dotenv").config();
+
+const userCollection = database
+  .db(process.env.MONGODB_DATABASE)
+  .collection("users");
 
 // render the reset password page
 router.get("/resetPassword", (req, res) => {
   res.render("resetPassword");
 });
 
-async function main() {
-  // create a transporter using Gmail SMTP
+router.post("/resetPassword", async (req, res) => {
+  const { recipient } = req.body;
+  const user = await userCollection.findOne({ email: recipient });
+  if (!user) {
+    // Email address not found in the database
+    const errorMessage =
+      "The email address is not registered. Please try again or sign up to create an account.";
+    const signupLink = "/signup";
+    res.render("resetPassword", { errorMessage, signupLink });
+    return;
+  }
+
+  try {
+    await sendEmail(recipient);
+    res.send(
+      "An email with instructions to reset your password has been sent to your registered email address. Please check your inbox and follow the provided link to reset your password."
+    );
+  } catch (error) {
+    console.error(error);
+    const errorMessage = "Failed to send email. Please try again later.";
+    res.render("resetPassword", { errorMessage });
+  }
+});
+
+async function sendEmail(recipient) {
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -20,24 +48,15 @@ async function main() {
     },
   });
 
-  // define and send message inside transporter.sendEmail() and await info about send from promise:
   let info = await transporter.sendMail({
-    from: `"DTC 15" <${process.env.EMAIL_ADDRESS}>`,
-    to: "comp2800dtc15@gmail.com",
-    subject: "test resetting password link",
+    from: `"Techommend" <${process.env.EMAIL_ADDRESS}>`,
+    to: recipient,
+    subject: "Password Reset Request",
     html: `
-    <h1>Hello</h1>
-    <p>test </p>
-    `,
+        <h1>Techommend</h1>
+        <p>To reset your password, click on the link below:</p>
+      `,
   });
-
-  console.log(info.messageId); // log message id
 }
-
-main().catch((err) => console.log(err)); // catch any errors
-
-router.post("/resetPassword", async (req, res) => {
-  const { email } = req.body;
-});
 
 module.exports = router;
