92943ae29f099bd94f2ec2b75bcd19b08e4fbcc3
Joanne Ho <cyh66368@gmail.com>
2023-05-10 23:30:52 -0700

    create a transporter using Gmail SMTP

--- resetPassword.js
+++ resetPassword.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
+// render the reset password page
 router.get("/resetPassword", (req, res) => {
   res.render("resetPassword");
+});
+
+// create a transporter using Gmail SMTP
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+router.post("/resetPassword", async (req, res) => {
+  const { email } = req.body;
 });
 
 module.exports = router;
