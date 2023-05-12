f29e71858096c6455e2e64a52f6d7ac47d0c7294
Joanne Ho <cyh66368@gmail.com>
2023-05-11 17:20:37 -0700

    Add a new route for handling the reset link and update user password

--- resetPassword.js
+++ resetPassword.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 const NodeCache = require("node-cache");
-const cache = new NodeCache();
+const cache = new NodeCache({ checkperiod: 1200 });
 const { database } = require("./databaseConnection");
 require("dotenv").config();
 
@@ -17,12 +17,30 @@ router.get("/resetPassword", (req, res) => {
   res.render("resetPassword");
 });
 
+// handle the password reset link
+router.get("/resetPassword/:token", (req, res) => {
+  const { token } = req.params;
+  const recipient = cache.get(token);
+  console.log("Token:", token);
+  console.log("Recipient:", recipient);
+
+  if (!recipient) {
+    // invalid or expired token
+    const errorMessage = "Invalid or expired password reset link.";
+    res.render("resetPassword", { errorMessage });
+    return;
+  }
+
+  // render the password reset form
+  res.render("resetPassword", { token });
+});
+
 router.post("/resetPassword", async (req, res) => {
   const { recipient } = req.body;
   const user = await userCollection.findOne({ email: recipient });
 
   if (!user) {
-    // Email address not found in the database
+    // email address not found in the database
     const errorMessage =
       "The email address is not registered. Please try again or sign up to create an account.";
     const signupLink = "/signup";
@@ -34,7 +52,6 @@ router.post("/resetPassword", async (req, res) => {
     const currentUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
     const uniqueString = uuidv4();
     cache.set(uniqueString, recipient, 60 * 60 * 1000); // cache for 1 hour
-    //console.log("currentURL:", currentUrl);
     await sendEmail(recipient, currentUrl, uniqueString);
     res.send(
       "An email with instructions to reset your password has been sent to your registered email address. Please check your inbox and follow the provided link to reset your password."
@@ -44,6 +61,35 @@ router.post("/resetPassword", async (req, res) => {
     const errorMessage = "Failed to send email. Please try again later.";
     res.render("resetPassword", { errorMessage });
   }
+});
+
+// handle the password reset form submission
+router.post("/resetPassword/:token", async (req, res) => {
+  const { token } = req.params;
+  const recipient = cache.get(token);
+
+  if (!recipient) {
+    // Invalid or expired token
+    const errorMessage = "Invalid or expired password reset link.";
+    res.render("resetPassword", { errorMessage });
+    return;
+  }
+
+  const { newPassword } = req.body;
+  const hashedPassword = await bcrypt.hash(newPassword, 12);
+  console.log("New Password:", newPassword);
+  console.log("Hashed Password:", hashedPassword);
+
+  // update the user password in the database
+  await userCollection.updateOne(
+    { email: recipient },
+    { $set: { password: hashedPassword } }
+  );
+
+  cache.del(token);
+
+  // redirect to login page
+  res.redirect("/login");
 });
 
 async function sendEmail(recipient, currentUrl, uniqueString) {
@@ -64,7 +110,7 @@ async function sendEmail(recipient, currentUrl, uniqueString) {
     html: `
         <h1>Techommend</h1>
         <p>To reset your password, click on the link below:</p>
-        <a href="${currentUrl}/user/${uniqueString}">Reset Password</a>
+        <a href="${currentUrl}/${uniqueString}">Reset Password</a>
       `,
   });
 }
