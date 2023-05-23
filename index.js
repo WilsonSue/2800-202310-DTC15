c49dc4ae1d30ba3976c3b00fc1104af34bf986eb
Joanne Ho <cyh66368@gmail.com>
2023-05-22 19:25:20 -0700

    moved css, audio, and image files to public directory for static file serving

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.use(
 app.use(resetPasswordRouter);
 app.use(savedListingsRouter);
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
 
 require("./routes")(app, userCollection, jobCollection, fakeJobsCollection);
 
