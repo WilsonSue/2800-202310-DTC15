6795872580ac4a03de7515c0a4412968379d2859
Elijah_Fabon <efabon2@my.bcit.ca>
2023-05-25 16:07:35 -0700

    move profile route from routes.js to profile.js

---
+++ routes/profile.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+router.get("/userProfile", async (req, res) => {
+    if (req.session.authenticated) {
+      const email = req.session.email;
+      const user = await userCollection.findOne({ email: email });
+
+      if (!user) {
+        res.redirect("/login");
+        return;
+      }
+
+      res.render("userProfile", { user });
+    } else {
+      res.redirect("/");
+      return;
+    }
+  });
+
+  router.post("/userProfile", async (req, res) => {
+    if (req.session.authenticated) {
+      const email = req.session.email;
+      const firstName = req.body.firstName;
+      const lastName = req.body.lastName;
+      const username = req.body.username;
+      const newEmail = req.body.email;
+      const password = req.body.password;
+      const skills = req.body.skills;
+      console.log(skills);
+      const personality = req.body.personality;
+
+      const updateFields = {
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        email: newEmail,
+        skills: skills,
+        personality: personality,
+      };
+
+      if (password && password.trim() !== "") {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        updateFields.password = hashedPassword;
+      }
+
+      await userCollection.updateOne({ email: email }, { $set: updateFields });
+
+      // Update session email if it changed
+      if (email !== newEmail) {
+        req.session.email = newEmail;
+      }
+
+      res.redirect("/userProfile");
+    } else {
+      res.redirect("/signup");
+      return;
+    }
+  });
