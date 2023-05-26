3e4aa6078fd0e30ff6423a10bb68a8b1c6e513f2
Elijah_Fabon <efabon2@my.bcit.ca>
2023-05-25 17:14:45 -0700

    link easterEgg route to index.js

--- routes/easterEgg.js
+++ routes/easterEgg.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-router.get("/easterEgg", async (req, res) => {
+module.exports = function (app, fakeJobsCollection) {
+  app.get("/easterEgg", async (req, res) => {
     // Fetch all the fake job listings
     const fakeJobs = await fakeJobsCollection.find().toArray();
 
     // Render the easterEgg view, passing in the fake job listings
     res.render("easterEgg", { fakeJobs });
   });
+}
